99bbd331ca51626c20be8bd9c83e874513f00cdb
Emily Wells Jimenez and Kyle Ebersole <kyle@>
2017-10-12 11:28:37 -0700

    Create functional to-do list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
 
-namespace NewProject.Controllers
+namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
@@ -8,6 +11,25 @@ namespace NewProject.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet("/task/list")]
+        public ActionResult TaskList()
+        {
+          List<string> allTasks = Task.GetAll();
+          return View(allTasks);
+        }
+        [HttpPost("/task/create")]
+        public ActionResult CreateTask()
+        {
+          Task newTask = new Task (Request.Form["new-task"]);
+          newTask.Save();
+          return View(newTask);
+        }
+        [HttpPost("/task/list/clear")]
+        public ActionResult TaskListClear()
+        {
+          Task.ClearAll();
+          return View();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace NewProject
+namespace ToDoList
 {
     public class Program
     {
